177d4e93725556f0d4d9c42874a2da6af7e99319
WebDeveloperTom <tom@webdevelopertom.com>
2019-02-13 11:49:54 +1300

    basic nav outline

---
+++ components/Nav.php
@@ -0,0 +1,25 @@
+<?php
+echo '
+  <header>
+    <div class="logo">
+      <img style="height: 100px;" src="assests\real-estate-logo.png" alt="" />
+    </div>
+    <nav class="main-menu">
+      <ul>
+        <li>Listings</li>
+        <li>Agents</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+    <nav class="login">
+      <ul>
+        <li>Admin</li>
+        <li>Sign Up</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  </header>
+';
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Real Estate</title>
+  </head>
+  <body>
+    <?php include './components/Nav.php'; ?>
+  </body>
+</html>
