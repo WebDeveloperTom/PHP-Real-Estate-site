ab1493f81259e65ead0a8d4e223bf53ef82c7c25
WebDeveloperTom <tom@webdevelopertom.com>
2019-02-19 11:47:03 +1300

    add basic styling

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>Real Estate</title>
+    <link rel="stylesheet" href="styles/nav.css">
+    <link rel="stylesheet" href="styles/index.css">
   </head>
   <body>
     <?php include './components/Nav.php'; ?>
+    <main>
+      <div class="cover">
+        <img  src="assests\main-cover-photo.jpeg" alt="">
+      </div>
+
+    </main>
   </body>
 </html>
