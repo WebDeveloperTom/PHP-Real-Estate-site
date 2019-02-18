5c9062e8b66f814b3b846ade45c28d1df0172962
WebDeveloperTom <tom@webdevelopertom.com>
2019-02-19 11:49:22 +1300

    added image to nav

--- components/Nav.php
+++ components/Nav.php
@@ -2,22 +2,24 @@
 echo '
   <header>
     <div class="logo">
-      <img style="height: 100px;" src="assests\real-estate-logo.png" alt="" />
+      <img src="assests\real-estate-logo.png" alt="" />
     </div>
-    <nav class="main-menu">
-      <ul>
-        <li>Listings</li>
-        <li>Agents</li>
-        <li>Contact Us</li>
-      </ul>
-    </nav>
-    <nav class="login">
-      <ul>
-        <li>Admin</li>
-        <li>Sign Up</li>
-        <li>Login</li>
-      </ul>
-    </nav>
+    <div class="menus">
+      <nav class="main-menu">
+        <ul>
+          <li>Listings</li>
+          <li>Agents</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+      <nav class="login">
+        <ul>
+          <li>Admin</li>
+          <li>Sign Up</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </div>
   </header>
 ';
 
