e5a5fea9a67bcd7156e3c1b450c5e0bd405f8ec4
WebDeveloperTom <tom@webdevelopertom.com>
2019-02-20 21:57:53 +1300

    merge Nav and Header component

---
+++ components/header.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+echo "
+<!DOCTYPE html>
+<html lang='en' dir='ltr'>
+  <head>
+    <meta charset='utf-8'>
+    <title></title>
+    <link rel='stylesheet' href='../styles/nav.css'>
+  </head>
+  <body>
+    <header>
+      <div class='logo'>
+        <img src='../components/real-estate-logo.png' alt='' />
+      </div>
+      <div class='menus'>
+        <nav class='main-menu'>
+          <ul>
+            <li>Listings</li>
+            <li>Agents</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        <nav class='login'>
+          <ul>
+            <li>Admin</li>
+            <li>Sign Up</li>
+            <li>Login</li>
+          </ul>
+        </nav>
+      </div>
+    </header>";
+ ?>
