9c1bc156245cfc3292828ae12d776173980f5009
WebDeveloperTom <tom@webdevelopertom.com>
2019-02-26 10:49:09 +1300

    fixed styling errors

--- components/header.php
+++ components/header.php
@@ -6,7 +6,9 @@ echo "
   <head>
     <meta charset='utf-8'>
     <title></title>
-    <link rel='stylesheet' href='../styles/nav.css'>
+    <link rel='stylesheet' href='/assignment7/styles/index.css'>
+    <link rel='stylesheet' href='/assignment7/styles/nav.css'>
+    <link rel='stylesheet' href='/assignment7/styles/properties.css'>
   </head>
   <body>
     <header>
@@ -16,16 +18,25 @@ echo "
       <div class='menus'>
         <nav class='main-menu'>
           <ul>
-            <li>Listings</li>
-            <li>Agents</li>
+            <li><a href='/assignment7/properties'>Listings</a></li>
+            <li><a href='/assignment7/agents'>Agents</a></li>
             <li>Contact Us</li>
           </ul>
         </nav>
         <nav class='login'>
-          <ul>
-            <li>Admin</li>
-            <li>Sign Up</li>
-            <li>Login</li>
+          <ul>";
+          //if admin
+          if (isset($_SESSION['admin'])) {
+            echo "<li> <a href='/assignment7/admin/'>Admin Panel</a></li>
+                  <li><a href='/assignment7/logout.php'>Sign Out</a></li>";
+          } elseif (isset($_SESSION['loggedIn'])) {
+            echo "<li><a href='/assignment7/wishlist/'>Wish List</a></li>
+                  <li><a href='/assignment7/logout.php'>Sign Out</a></li>";
+          } else {
+            echo "<li><a href='/assignment7/signup.php'>Sign Up </a></li>
+                  <li><a href='/assignment7/login.php'>Login</a></li>";
+          }
+            echo "
           </ul>
         </nav>
       </div>
