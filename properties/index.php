23a8d7421b297efe3935fc9111e32efab267a9aa
WebDeveloperTom <tom@webdevelopertom.com>
2019-02-26 10:56:46 +1300

    Added conditions to display correct data

--- properties/index.php
+++ properties/index.php
@@ -1,5 +1,10 @@
 <?php include '../components/header.php'; ?>
-<?php include '../includes/config.php';
+<?php include '../includes/config.php'; ?>
+<div class="houses-main">
+
+
+<?php
+//need db connection check.
     if (true) {
       $sql = "SELECT * FROM houses";
       $result = mysqli_query($conn, $sql);
@@ -13,12 +18,17 @@
           </div>
           <div class='house-detail'>
             <a href='./listing.php?id=$row[house_id]'>
-              <p class='address'> $row[address_1], $row[address_2] </p>
-              <p class='price'></p>
+              <p class='address'> $row[address_1], $row[address_2] </p>";
+              if ($row['auction']) {
+                echo "<p class='auction'>To be auctioned</p>";
+              } else {
+                echo "<p class='price'>$$row[price]</p>";
+              }
+              echo "
               <div class=''>
-                <span class='bed'></span>
-                <span class='bath'></span>
-                <span class='car'></span>
+                <span class='bed'>Bed: $row[bed]</span>
+                <span class='bath'>Bath: $row[bathroom]</span>
+                <span class='car'>Car: $row[car]</span>
               </div>
             </a>
           </div>
@@ -31,4 +41,5 @@
       echo "WHoops, something went wrong";
     }
      ?>
+</div>
 <?php include '../components/footer.php'; ?>
