167a4f15f7daee1ec32b08619777b5100f69e212
WebDeveloperTom <tom@webdevelopertom.com>
2019-03-11 14:39:25 +1300

    update styling and layout of edit page

--- admin/edit.php
+++ admin/edit.php
@@ -1,7 +1,18 @@
 <?php
     include '../components/header.php';
     include '../includes/config.php';
-    // echo "This is the property page";
+    ?>
+<div class="edit-main">
+  <?php
+  if (!isset($_SESSION["admin"])) {
+    echo "Youre not the admin";
+    exit();
+  } ?>
+  <div class="edit-form">
+
+
+
+<?php
     if (isset($_GET['id'])) {
       $id = $_GET['id'];
       $sql = "SELECT * FROM houses WHERE house_id = '$id'";
@@ -11,27 +22,67 @@
           <form action='";
         echo htmlspecialchars('../includes/editAction.php');
         echo "' method='POST' enctype='multipart/form-data'>
-            <input type='text' name='house_id' readonly value='$row[house_id]' />
-            <input type='text' name='address_1' value='$row[address_1]' />
-            <input type='text' name='address_2' value='$row[address_2]' />
-            <input type='text' name='bed' value='$row[bed]' />
-            <input type='text' name='bathroom' value='$row[bathroom]' />
-            <input type='text' name='car' value='$row[car]' />
-            <textarea name='description' id='' cols='30' rows='30'>$row[description]</textarea>
-            <input type='text' name='price' value='$row[price]' />";
+          <ul class='form-flex'>
+            <input type='hidden' name='house_id' readonly value='$row[house_id]' />
+            <li>
+              <label for='address_1'>Adress line 1</label>
+              <input type='text' name='address_1' value='$row[address_1]' />
+            </li>
+            <li>
+              <label for='address_2'>Adress line 2</label>
+              <input type='text' name='address_2' value='$row[address_2]' />
+            </li>
+            <li>
+              <label for='bed'>Number of Beds</label>
+              <input type='text' name='bed' value='$row[bed]' />
+            </li>
+            <li>
+              <label for='bathroom'>Number of Bathrooms</label>
+              <input type='text' name='bathroom' value='$row[bathroom]' />
+            </li>
+            <li>
+              <label for='car'>Number of Car parks</label>
+              <input type='text' name='car' value='$row[car]' />
+            </li>
+            <li>
+              <label for='description'>House description</label>
+              <textarea name='description' id='' rows='6'>$row[description]</textarea>
+            </li>
+            <li>
+              <label for='price'>Price</label>
+              <input type='text' name='price' value='$row[price]' />
+            </li>";
+
+
+
         if ($row['auction']) {
           echo "
-            <input type='checkbox' name='auction' checked value='1' />";
+          <li>
+            <input type='checkbox' name='auction' checked value='1' />
+            <p>Auction?</p>
+          </li>";
         } else {
           echo "
-            <input type='checkbox' name='auction' />";
+          <li>
+            <input type='checkbox' name='auction' />
+            <label for='auction'>Auction?</label>
+          </li>";
         }
         echo "
-            <input type='file' name='file' />
-            <img src='../house_assests/$row[image_link]' alt='' />
+            <li>
+              <label for='file'>Upload an image</label>
+              <input type='file' name='file' />
+            </li>
+
+            <img class='current-img' src='../house_assests/$row[image_link]' alt='' />
+            <li>
+              <label for='agent_id'>Agent</label>
+              <input type='text' name='agent_id' value='$row[agent_id]' />
+            </li>
             <input type='hidden' name='image_link' value='$row[image_link]' />
-            <input type='text' name='agent_id' value='$row[agent_id]' />
+
             <button type='submit'>Update</button>
+            </ul>
           </form>
         ";
         echo "
@@ -48,5 +99,9 @@
     } else {
       echo "WHoops, something went wrong";
     }
+    ?>
+      </div>
+</div>
+<?php
      include '../components/footer.php';
 ?>
