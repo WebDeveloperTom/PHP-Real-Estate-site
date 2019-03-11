72ec1bfa60d18333f012c909abf87a5acfe8ea26
WebDeveloperTom <tom@webdevelopertom.com>
2019-03-11 14:38:14 +1300

    moved login to it's own component

--- login.php
+++ login.php
@@ -1,23 +1,12 @@
 <?php include 'components/header.php'; ?>
     <main>
-      <form class="" action="<?php echo htmlspecialchars('includes\loginAction.php')?>" method="post">
-        <input type="text" name="username" value="" placeholder="Username">
-        <input type="password" name="pwd" value="" placeholder="Password">
-        <button type="submit" name="button">Login</button>
-      </form>
       <?php
-      if (!isset($_SESSION['loggedIn'])) {
-        echo "You are not logged in";
+      if (isset($_SESSION['loggedIn'])) {
+        echo "You are already logged in";
       } else {
-        echo "You ARE logged in";
-      }
-      if ($_SESSION['admin']) {
-        echo "You are an admin";
-      } else {
-        echo "You are not admin";
-      }
+          include 'components\loginForm.php';
 
-
-       ?>
+      }
+?>
     </main>
 <?php include 'components/footer.php'; ?>
